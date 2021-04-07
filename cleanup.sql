2318b6535c76b3e3109c83cce22edf0efbf70d0a
Alex Hepp <ahepp1999@gmail.com>
2021-04-06 21:48:39 -0400

    Finished set-up scripts

---
+++ cleanup.sql
@@ -0,0 +1,5 @@
+drop table if exists Coach;
+drop table if exists Stat;
+drop table if exists Demographic;
+drop table if exists Team;
+drop table if exists Award;
--- setup.sql
+++ setup.sql
@@ -14,14 +14,14 @@ primary key (season));
 create table Team
 (season varchar(15),
 team varchar(5),
-team_name varchar(30),
+team_name varchar(40),
 W int,
 L int,
 SRS float,
 pace float,
 ortg float,
 drtg float,
-topWS varchar(30),
+topWS varchar(50),
 conference varchar(10),
 playoffs int,
 primary key (season, team),
@@ -34,7 +34,7 @@ player_position varchar(5),
 height int,
 weight int,
 birth_date date,
-primary key (name));
+primary key (name, birth_date));
 
 create table Stat
 (season varchar(15),
@@ -55,9 +55,9 @@ STL int,
 BLK int,
 TOV int,
 PTS int,
-primary key (season, name),
+primary key (season, name, team),
 foreign key (name) references Demographic(name),
-foreign key (season) references Award(season));
+foreign key (season, team) references Team(season, team));
 
 create table Coach
 (season varchar(15),
@@ -68,32 +68,31 @@ foreign key (season, team) references Team(season, team));
 
 -- Loading
 
-LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/smalls/awards_small.csv'
+LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/awards.csv'
 INTO TABLE Award 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-
-LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/smalls/team_stats_small.csv'
+LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/team_stats.csv'
 INTO TABLE Team 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/smalls/player_demographics_small.csv'
+LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/player_demographics.csv'
 INTO TABLE Demographic 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/smalls/player_stats_small.csv'
+LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/player_stats.csv'
 INTO TABLE Stat 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/smalls/coaches_small.csv'
+LOAD DATA LOCAL INFILE '/Users/alexanderhepp/Documents/Databases/Project/nba-database/coaches.csv'
 INTO TABLE Coach 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
