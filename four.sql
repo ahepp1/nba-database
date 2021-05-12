3ca11e506b23ed5ba27ae1f9955d4626283f9f49
Nicola Ivanov <42846081+nicanov4@users.noreply.github.com>
2021-05-11 20:18:24 -0400

    Add files via upload
    Fixed 4

--- four.sql
+++ four.sql
@@ -5,32 +5,16 @@ DROP PROCEDURE IF EXISTS Four //
 CREATE PROCEDURE Four(IN s VARCHAR(40))
 BEGIN
         
-select S.season, S.team, D.name, D.height, PTS AS category
+select * from (
+select S.season, S.team, D.name, D.height, PTS
 from Stat as S, Demographic as D
-where S.name = D.name and PTS IN (
-        select case s
-	       
-        WHEN '3P' THEN 3P
-        WHEN '3PA' THEN 3PA
-        WHEN 'FG' THEN FG
-       WHEN 'FGA' THEN FGA
-       WHEN 'FT' THEN FT
-       WHEN 'FTA' THEN FTA
-            
-       WHEN 'ORB' THEN ORB
-       WHEN 'DRB' THEN DRB
-       WHEN 'AST' THEN AST
-       WHEN 'STL' THEN STL
-       WHEN 'BLK' THEN BLK
-       WHEN 'TOV' THEN TOV
-       WHEN 'PTS' THEN PTS
-       
-       ELSE NULL
-       END
+where S.name = D.name and PTS = (
+        select max(PTS) 
         from Stat as S2 
         where S.season = S2.season)
 group by season
-order by D.height
+order by D.height) a
+where team = s
 limit 1;
 
 END; //
